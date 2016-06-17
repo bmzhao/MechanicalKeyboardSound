3cbc179e22a2bc60de19726d5b8c6a7f1167e1d8
Brian Zhao <bzhao@jpl.nasa.gov>
2016-06-16 19:30:58 -0700

    init

---
+++ src/main/java/Mechanical.java
@@ -0,0 +1,51 @@
+import javazoom.jl.player.Player;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.io.FileInputStream;
+
+public class Mechanical {
+
+    public static void main(String[] args) {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            System.err.println("There was a problem registering the native hook.");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new NativeKeyListener() {
+            public void nativeKeyReleased(NativeKeyEvent e) {
+                System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+            }
+
+            public void nativeKeyPressed(NativeKeyEvent e) {
+                System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+                if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+                    try {
+                        GlobalScreen.unregisterNativeHook();
+                    } catch (NativeHookException e1) {
+                        e1.printStackTrace();
+                    }
+                } else {
+                    try {
+                        FileInputStream fis = new FileInputStream("key.mp3");
+                        Player playMP3 = new Player(fis);
+                        playMP3.play(10);
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+
+
+            public void nativeKeyTyped(NativeKeyEvent e) {
+                System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+            }
+        });
+    }
+}
